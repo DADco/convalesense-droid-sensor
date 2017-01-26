253ddf80ac31b444ceb50fb5b9947a8dc67ebd7a
PorridgeBear <allistair@gmail.com>
2017-01-26 16:34:34 +0000

    Add finger tap scoring.

--- app/src/main/java/co/dad/convalesensechild/GameFingerStrength.java
+++ app/src/main/java/co/dad/convalesensechild/GameFingerStrength.java
@@ -1,9 +1,14 @@
 package co.dad.convalesensechild;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.TextView;
 
 public class GameFingerStrength extends GameBase {
+
+    static String TAG = "GameFingerStrength";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +19,14 @@ public class GameFingerStrength extends GameBase {
 
         TextView textView = (TextView) findViewById(R.id.instruction);
         textView.setText(R.string.instruction_finger_strength);
+
+        View view = (View) findViewById(R.id.activity_game_finger_strength);
+        view.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                Log.d(TAG, "score");
+                return false;
+            }
+        });
     }
 }
